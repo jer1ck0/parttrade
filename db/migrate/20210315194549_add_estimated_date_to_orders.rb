0ebb4071365e5738fdf89d54f5a6049cd0ba750e
root <root@lex.lebedev.example.com>
2021-08-24 21:02:47 +0300

    ЭЭsome fix for deploy

--- db/migrate/20210315194549_add_estimated_date_to_orders.rb
+++ db/migrate/20210315194549_add_estimated_date_to_orders.rb
@@ -1,5 +1,5 @@
 class AddEstimatedDateToOrders < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :estimated_date, :datestamp
+    add_column :orders, :estimated_date, :timestamp
   end
 end
